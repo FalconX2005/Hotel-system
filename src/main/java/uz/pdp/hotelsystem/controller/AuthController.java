e00e3de0e9367ea1a625cd1c96b3383cbefe8888
qwerty <qwerty@gmail.com>
2025-02-03 15:27:53 +0500

    login

--- src/main/java/uz/pdp/hotelsystem/controller/AuthController.java
+++ src/main/java/uz/pdp/hotelsystem/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.hotelsystem.entity.User;
+import uz.pdp.hotelsystem.payload.LoginDTO;
 import uz.pdp.hotelsystem.payload.SignUpDTO;
 import uz.pdp.hotelsystem.repository.UserRepository;
 
@@ -23,6 +24,35 @@ public class AuthController {
     public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO loginDTO,HttpServletRequest request) {
+        Optional<User> userByUsername = userRepository.getUserByUsername(loginDTO.getUsername());
+        if(!userByUsername.isPresent()){
+            throw new RuntimeException("User not found");
+        }
+        User user = userByUsername.get();
+        if(!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())){
+            throw new RuntimeException("Wrong password");
+        }
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                user.getAuthorities()
+        );
+
+
+        securityContext.setAuthentication(authentication);
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+
+        return "success";
+
+
     }
 
     @PostMapping("/sign-up")
