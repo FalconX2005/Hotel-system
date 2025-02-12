3c2588a5ce6cba199506919ee70f4c56bc21a830
Timati <timatiborisov13@gmail.com>
2025-02-12 13:27:58 +0500

    security reset password

--- src/main/java/uz/pdp/hotelsystem/service/UserService.java
+++ src/main/java/uz/pdp/hotelsystem/service/UserService.java
@@ -1,6 +1,7 @@
 package uz.pdp.hotelsystem.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import uz.pdp.hotelsystem.entity.User;
 import uz.pdp.hotelsystem.repository.UserRepository;
 
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService  {
 
     private final UserRepository userRepository;
